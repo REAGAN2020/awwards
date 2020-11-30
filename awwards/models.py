a0f961fb0f513812cc3aa1d13457fee2a5df720a
REAGAN2020 <reaganteflon@gmail.com>
2020-11-30 11:31:52 +0300

    added model profile

--- awwards/models.py
+++ awwards/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Profile(models.Model):
+    Profile_pic = models.ImageField(upload_to='profile_pics/', blank=True)
+    prof_user = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
