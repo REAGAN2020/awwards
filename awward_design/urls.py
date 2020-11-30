fc2bf3857bc749162909126008e3814113784d7d
REAGAN2020 <reaganteflon@gmail.com>
2020-11-30 14:04:05 +0300

    added login and registration template

--- awward_design/urls.py
+++ awward_design/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^awwards/', include('awwards.urls')),
+    url(r'', include('awwards.urls')),
 ]
