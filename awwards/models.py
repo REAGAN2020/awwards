d82ffd99966d09648a37f4663c288a275c72db89
REAGAN2020 <reaganteflon@gmail.com>
2020-11-30 11:51:11 +0300

    added some zip code in the field for design and etc

--- awwards/models.py
+++ awwards/models.py
@@ -37,5 +37,9 @@ class Project(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
     image = models.ImageField(upload_to='project_images', blank=True)
     user_project_id = models.IntegerField(default=0)
-
+    design = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    usability = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    creativity = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    content = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    vote_submissions = models.IntegerField(default=0)
 
